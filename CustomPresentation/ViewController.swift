d60ff3b5989cd891cb486e72eaf42c5f4a79432c
Deepika <deepika.ram@anywhere.co>
2020-07-30 19:09:15 +0530

    Trying to handle pan gesture

--- CustomPresentation/ViewController.swift
+++ CustomPresentation/ViewController.swift
@@ -30,7 +30,8 @@ class ViewController: UIViewController {
        let view = UIView()
         view.backgroundColor = .green
         view.translatesAutoresizingMaskIntoConstraints = false
-        view.addGestureRecognizer(self.tapRecognizer)
+//        view.addGestureRecognizer(self.tapRecognizer)
+        view.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(popViewPanned(recognizer:))))
         return view
     }()
     
@@ -38,7 +39,7 @@ class ViewController: UIViewController {
     
     private lazy var tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(didTapPopView))
     
-    private lazy var popViewHeightConstraint: NSLayoutConstraint = self.popupView.heightAnchor.constraint(equalToConstant: 60)
+    private lazy var popViewBottomConstraint: NSLayoutConstraint = self.popupView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: 440)
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -48,30 +49,68 @@ class ViewController: UIViewController {
     
     private func activateConstraints() {
         NSLayoutConstraint.activate([
-            popupView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+            popViewBottomConstraint,
             popupView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             popupView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            popViewHeightConstraint
+            popupView.heightAnchor.constraint(equalToConstant: 500)
         ])
     }
 
     @objc private func didTapPopView() {
         
+       _ = animateTransitionIfNeeded()
+    }
+    
+    func animateTransitionIfNeeded() -> UIViewPropertyAnimator {
         let state = currentState.opposite
         let transitionAnimator = UIViewPropertyAnimator(duration: 1, dampingRatio: 1, animations: {
             switch state {
             case .open:
-                self.popViewHeightConstraint.constant = 500
+                self.popViewBottomConstraint.constant = 0
             case .closed:
-                self.popViewHeightConstraint.constant = 60
+                self.popViewBottomConstraint.constant = 440
             }
             self.view.layoutIfNeeded()
         })
-        
         transitionAnimator.addCompletion { position in
-            self.currentState = state
+            switch position {
+            case .start:
+                self.currentState = state.opposite
+            case .end:
+                self.currentState = state
+            case .current:
+                ()
+            @unknown default:
+                assertionFailure("Unknown case")
+            }
+            switch self.currentState {
+            case .open:
+                self.popViewBottomConstraint.constant = 0
+            case .closed:
+                self.popViewBottomConstraint.constant = 440
+            }
         }
         transitionAnimator.startAnimation()
+        return transitionAnimator
+    }
+    
+    @objc private func popViewPanned(recognizer: UIPanGestureRecognizer) {
+        var transitionAnimator = UIViewPropertyAnimator()
+        switch recognizer.state {
+
+        case .began:
+            transitionAnimator = animateTransitionIfNeeded()
+            transitionAnimator.pauseAnimation()
+        case .changed:
+            let translation = recognizer.translation(in: popupView)
+            var fraction = -translation.y / popupView.frame.height
+            if currentState == .open { fraction *= -1 }
+            transitionAnimator.fractionComplete = fraction
+        case .ended:
+            transitionAnimator.continueAnimation(withTimingParameters: nil, durationFactor: 0)
+        default:
+            print("State not handled!")
+        }
     }
 
 }
